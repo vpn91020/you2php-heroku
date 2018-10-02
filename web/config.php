d301ab556ef0898c02b42486fca373f0f659e897
zs910208565 <34202810+zs910208565@users.noreply.github.com>
2018-10-02 20:09:35 +0800

    Update and rename web/config.php to  web/config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('APIKEY', 'AIzaSyBuU-yyGEA20KZ2n3Jc3cZ3GJHioFppA5c');//这里改成您的api key
+define('GJ_CODE', 'HK');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', '910208565@qq.com');//邮箱
 ?>
